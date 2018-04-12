a0eaa496523bc6f4b3d4e5f32db02675d04a2c30
Nick Lee <nickoless.lee@gmail.com>
2018-04-11 20:15:46 -0700

    Removed whitespace

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,11 @@
 $(() => {
-
   let getQuote = () => {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", (a) => {
       $(".message").html(a[0].content);
       $(".author").html(a[0].title)
     });
   };
-
+  
   getQuote();
 
   $('button').on('click', () => {
