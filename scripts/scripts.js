3b8f5aa3fe68435f4dc54da30445ff31a38eeed8
Nick Lee <nickoless.lee@gmail.com>
2018-04-11 20:14:20 -0700

    Cleaned up code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,16 @@
 $(() => {
 
-  let randomInt = undefined;
-  function getRandomInt() {
-    randomInt = Math.floor(Math.random() * Math.floor(3));
-    return randomInt;
-  }
-  
+  let getQuote = () => {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", (a) => {
+      $(".message").html(a[0].content);
+      $(".author").html(a[0].title)
+    });
+  };
+
+  getQuote();
 
   $('button').on('click', () => {
     event.preventDefault();
-    getRandomInt();
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $(".message").html(a[0].content + "<p>— " + a[0].title + "</p>");
-    });
+    getQuote();
   });
-    // $('.message, .context').animate({ opacity: 0 }, 1000, () => {
-    //   $('.message').html(quotes[randomInt].quote);
-    //   $('.context').html('TRUMP' + quotes[randomInt].context);
-    // }).animate({ opacity: 1 }, 1000);
-
-    // $('.message').html(quotes[randomInt].quote);
-    // $('.context').html(quotes[randomInt].context);
-
-
-
 });
